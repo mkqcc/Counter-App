94517c922fa50cfa70c52b0529cb03bbd64de1c7
mkqcc <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-07-27 15:34:09 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Counter extends Component {
+    constructor() {
+        super()
+        this.state = {
+            count: 0,
+        }
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="navbar">Counter.js</div>
+                <div className="counter">
+                    <h1>{this.state.count}</h1>
+                    <button type="button" onClick={this.increment}>Increment</button>
+                    <button type="button" onClick={this.decrement}>Decrement</button>
+                    <button type="button" onClick={this.reset}>Clear</button>
+                </div>
+            </div>
+        )
+    }
+
+    increment = () => {
+        this.setState({count: this.state.count + 1})
+    }
+
+    decrement = () => {
+        if (this.state.count > 0) {
+            this.setState({count: this.state.count - 1})
+        }
+    }
+
+    reset = () => {
+        this.setState({count: 0})
+    }
 }
 
-export default App;
+export default Counter
+
