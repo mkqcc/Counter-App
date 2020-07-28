1d84cbd7c34325ff2ca14da833aee76a699de8b6
mkqcc <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-07-28 14:59:14 -0400

    Add new features

--- src/App.js
+++ src/App.js
@@ -5,37 +5,56 @@ class Counter extends Component {
         super()
         this.state = {
             count: 0,
+            doubleCount: false,
         }
     }
 
     render() {
         return (
             <div className="container">
-                <div className="navbar">Counter.js</div>
+                <div className="navbar">Counter.JS</div>
                 <div className="counter">
                     <h1>{this.state.count}</h1>
                     <button type="button" onClick={this.increment}>Increment</button>
                     <button type="button" onClick={this.decrement}>Decrement</button>
                     <button type="button" onClick={this.reset}>Clear</button>
+                    <button type="button" onClick={this.double}>
+                        {this.state.doubleCount ? 'Double Count' : 'Single Count'}
+                    </button>
                 </div>
             </div>
         )
     }
 
     increment = () => {
-        this.setState({count: this.state.count + 1})
+        if (this.state.count < 20) {
+            this.setState({count: this.state.count + 1})
+        }
+        if (this.state.doubleCount && this.state.count < 19) {
+            this.setState({count: this.state.count + 2,})
+        }
     }
 
     decrement = () => {
         if (this.state.count > 0) {
             this.setState({count: this.state.count - 1})
         }
+        if (this.state.doubleCount && this.state.count > 1) {
+            this.setState({count: this.state.count - 2,})
+        }
     }
 
     reset = () => {
         this.setState({count: 0})
     }
+
+    double = () => {
+        if (this.state.doubleCount) {
+            this.setState({doubleCount: false})
+        } else {
+            this.setState({doubleCount: true})
+        }
+    }
 }
 
 export default Counter
-
